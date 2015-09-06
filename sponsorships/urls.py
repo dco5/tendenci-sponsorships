8829a9352f7658d0cf8179e703847847239eef89
dco5 <jlosada87@hotmail.com>
2015-09-06 18:13:48 -0500

    fix urls

--- sponsorships/urls.py
+++ sponsorships/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('sponsorships.views',
-    url(r'^sponsorships/$', 'add', name="sponsorships.add"),
-    url(r'^sponsorships/conf/(?P<id>\d+)/$', 'add_confirm', name="sponsorships.add_confirm"),
+    url(r'^sponsorships/$', 'add', name="sponsorship.add"),
+    url(r'^sponsorships/conf/(?P<id>\d+)/$', 'add_confirm', name="sponsorship.add_confirm"),
     url(r'^sponsorships/(?P<id>\d+)/$', 'detail', name="sponsorships.view"),
-    url(r'^sponsorships/receipt/(?P<id>\d+)/(?P<guid>[\d\w-]+)/$', 'receipt', name="sponsorships.receipt"),
-    url(r'^sponsorships/search/$', 'search', name="sponsorships.search"),
+    url(r'^sponsorships/receipt/(?P<id>\d+)/(?P<guid>[\d\w-]+)/$', 'receipt', name="sponsorship.receipt"),
+    url(r'^sponsorships/search/$', 'search', name="sponsorship.search"),
 )

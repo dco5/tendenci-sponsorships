6b1e45c260439130126f3a0557c96b653193e05a
dco5 <jlosada87@hotmail.com>
2015-09-06 19:55:35 -0500

    fix url sponsorship.view name

--- sponsorships/urls.py
+++ sponsorships/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('sponsorships.views',
     url(r'^sponsorships/$', 'add', name="sponsorship.add"),
     url(r'^sponsorships/conf/(?P<id>\d+)/$', 'add_confirm', name="sponsorship.add_confirm"),
-    url(r'^sponsorships/(?P<id>\d+)/$', 'detail', name="sponsorships.view"),
+    url(r'^sponsorships/(?P<id>\d+)/$', 'detail', name="sponsorship.view"),
     url(r'^sponsorships/receipt/(?P<id>\d+)/(?P<guid>[\d\w-]+)/$', 'receipt', name="sponsorship.receipt"),
     url(r'^sponsorships/search/$', 'search', name="sponsorship.search"),
 )

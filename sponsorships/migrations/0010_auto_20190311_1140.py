eae11a8fd0407ab4ef2dd8ec7122c566e72f8a40
jlossada <jlosada87@hotmail.com>
2019-03-11 14:06:48 -0500

    - removed dependency from events

--- sponsorships/migrations/0010_auto_20190311_1140.py
+++ sponsorships/migrations/0010_auto_20190311_1140.py
@@ -7,7 +7,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('events', '0008_auto_20190311_1140'),
         ('sponsorships', '0009_notifyeventadmin'),
     ]
 
